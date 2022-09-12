1204d737ecb9418be7bb79def87165caf152968c
Rosario <rosariodavi9@gmail.com>
2022-09-12 16:31:26 -0400

    Feature 7 complete

--- tracker/urls.py
+++ tracker/urls.py
@@ -21,5 +21,6 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
+    path("accounts/", include("accounts.urls")),
     path("", RedirectView.as_view(url="/projects/"), name="home"),
 ]
