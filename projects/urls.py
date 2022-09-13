7e362709c6c00223ea937758d1304fc60f4f776a
Rosario <rosariodavi9@gmail.com>
2022-09-13 12:40:42 -0400

    Feature 14 complete

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from projects.views import ProjectsListView, ProjectDetailView
+from projects.views import (
+    ProjectsListView,
+    ProjectDetailView,
+    ProjectCreateView,
+)
 
 urlpatterns = [
     path("", ProjectsListView.as_view(), name="list_projects"),
     path("<int:pk>/", ProjectDetailView.as_view(), name="show_project"),
+    path("create/", ProjectCreateView.as_view(), name="create_project"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from projects.models import Project
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 
 class ProjectsListView(LoginRequiredMixin, ListView):
@@ -15,3 +16,12 @@ class ProjectsListView(LoginRequiredMixin, ListView):
 class ProjectDetailView(LoginRequiredMixin, DetailView):
     model = Project
     template_name = "projects/detail.html"
+
+
+class ProjectCreateView(LoginRequiredMixin, CreateView):
+    model = Project
+    template_name = "projects/create.html"
+    fields = ["name", "description", "members"]
+
+    def get_success_url(self):
+        return reverse_lazy("show_project", args=[self.object.id])
