15ec488fd9d39356d98afc018a5a7d9ac4ff784d
Rosario <rosariodavi9@gmail.com>
2022-09-13 13:12:22 -0400

    Feature 15 complete

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from tasks.models import Task
 
-# Create your views here.
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    template_name = "tasks/create.html"
+    fields = ["name", "start_date", "due_date", "project", "assignee"]
+
+    def form_valid(self, form):
+        item = form.save(commit=False)
+        item.assignee = self.request.user
+        item.save()
+        return redirect("projects/detail.html")
--- tracker/urls.py
+++ tracker/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
     path("accounts/", include("accounts.urls")),
+    path("tasks/", include("tasks.urls")),
     path("", RedirectView.as_view(url="/projects/"), name="home"),
 ]
